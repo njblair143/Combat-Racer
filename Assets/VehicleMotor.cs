9e50d6a1d91fab39b335bd98704d49fd6a383e66
Pepper Passion <njblair143@gmail.com>
2017-10-27 15:50:48 -0500

    Can now move
    but it's reversed

--- Assets/VehicleMotor.cs
+++ Assets/VehicleMotor.cs
@@ -4,6 +4,35 @@
 
 public class VehicleMotor : MonoBehaviour {
 
+    private Vector3 velocity = Vector3.zero;
 
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Gets a movement vector
+    public void Move (Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    // Run every physics iteration
+    void FixedUpdate()
+    {
+        PerformMovement();
+    }
+
+    // Perform movement based on velocity variable
+    void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
 
 }
